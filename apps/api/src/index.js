a832249452ddaf8b4216ea23c2b148082f9a4e35
Simon Baev <simon@ccprotocol.xyz>
2025-10-12 16:07:07 -0600

    w: simple take 2

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,9 +1,9 @@
 import { register } from 'node:module'
 import { Worker } from 'node:worker_threads'
+import { workerFileName } from './worker.js'
 
 const init = async () => {
   register('esm-module-alias/loader', import.meta.url)
-  const { workerFileName } = await import('./worker.js')
   const worker = new Worker(workerFileName, {
     name: 'PIR',
   })
--- apps/api/src/worker.js
+++ apps/api/src/worker.js
@@ -1,7 +1,5 @@
 import { isMainThread, parentPort } from 'node:worker_threads'
-import i2c from '@/i2c-stub.js'
-import { sleep, buzzer } from '@/tools/index.js'
-export const workerFileName = new URL(import.meta.url)
+import { register } from 'node:module'
 
 const STATUS_BITS = {
   RAW: 0x01,
@@ -15,38 +13,42 @@ const MASKS = {
 const DEVICE_ADDR = 0x12
 const STATUS_REG = 0x03
 
-const bus = await i2c.openPromisified(1)
+const init = async () => {
+  register('esm-module-alias/loader', import.meta.url)
+  const i2c = await import('@/i2c-stub.js')
+  const { sleep, buzzer } = await import('@/tools/index.js')
+  const bus = await i2c.default.openPromisified(1)
 
-let active = false
+  let active = false
 
-const onActive = async () => {
-  await buzzer.runProfile([{ note: 'E5', length: 250 }], false)
-  await sleep(3000)
-}
+  const onActive = async () => {
+    await buzzer.runProfile([{ note: 'E5', length: 250 }], false)
+    await sleep(3000)
+  }
 
-const start = (interval) => {
-  const timer = setInterval(
-    async () => {
-      const status = await bus.readByte(DEVICE_ADDR, STATUS_REG)
-      if (status & STATUS_BITS.AVAILABLE) {
-        if (!active && (status & STATUS_BITS.DET)) {
-          active = true
-          onActive().then(
-            () => {
-              active = false
-            }
-          )
+  const start = (interval) => {
+    const timer = setInterval(
+      async () => {
+        const status = await bus.readByte(DEVICE_ADDR, STATUS_REG)
+        if (status & STATUS_BITS.AVAILABLE) {
+          if (!active && (status & STATUS_BITS.DET)) {
+            active = true
+            onActive().then(
+              () => {
+                active = false
+              }
+            )
+          }
+          await bus.writeByte(DEVICE_ADDR, STATUS_REG, status & (~MASKS.CLEAR))
         }
-        await bus.writeByte(DEVICE_ADDR, STATUS_REG, status & (~MASKS.CLEAR))
-      }
-    },
-    interval
-  )
-  return timer
-}
+      },
+      interval
+    )
+    return timer
+  }
 
-if (!isMainThread) {
   let timer = start(100)
+
   parentPort.on('message', (message) => {
     if (timer) {
       clearInterval(timer)
@@ -58,6 +60,11 @@ if (!isMainThread) {
       }
     }
   })
-
   console.log('Worker started')
 }
+
+if (!isMainThread) {
+  init()
+}
+
+export const workerFileName = new URL(import.meta.url)
