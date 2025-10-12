9db4ac5929640fdfbc9f1fc180d1c9060d95e5f9
Simon Baev <simon@ccprotocol.xyz>
2025-10-12 16:17:21 -0600

    w: simple take 4

--- apps/api/src/worker.js
+++ apps/api/src/worker.js
@@ -14,57 +14,57 @@ const MASKS = {
 const DEVICE_ADDR = 0x12
 const STATUS_REG = 0x03
 
-const init = async () => {
-  register('esm-module-alias/loader', import.meta.url)
-
-  const { sleep, buzzer } = await import('@/tools/index.js')
-  const bus = await i2c.default.openPromisified(1)
-
-  let active = false
-
-  const onActive = async () => {
-    await buzzer.runProfile([{ note: 'E5', length: 250 }], false)
-    await sleep(3000)
-  }
-
-  const start = (interval) => {
-    const timer = setInterval(
-      async () => {
-        const status = await bus.readByte(DEVICE_ADDR, STATUS_REG)
-        if (status & STATUS_BITS.AVAILABLE) {
-          if (!active && (status & STATUS_BITS.DET)) {
-            active = true
-            onActive().then(
-              () => {
-                active = false
-              }
-            )
-          }
-          await bus.writeByte(DEVICE_ADDR, STATUS_REG, status & (~MASKS.CLEAR))
-        }
-      },
-      interval
-    )
-    return timer
-  }
-
-  let timer = start(100)
-
-  parentPort.on('message', (message) => {
-    if (timer) {
-      clearInterval(timer)
-      if (message > 0) {
-        console.log(`Polling interval set to ${message} ms`)
-        timer = start(message)
-      } else {
-        console.log('Polling stopped')
-      }
-    }
-  })
-  console.log('Worker started')
-}
-
 if (!isMainThread) {
+  const init = async () => {
+    register('esm-module-alias/loader', import.meta.url)
+
+    const { sleep, buzzer } = await import('@/tools/index.js')
+    const bus = await i2c.default.openPromisified(1)
+
+    let active = false
+
+    const onActive = async () => {
+      await buzzer.runProfile([{ note: 'E5', length: 250 }], false)
+      await sleep(3000)
+    }
+
+    const start = (interval) => {
+      const timer = setInterval(
+        async () => {
+          const status = await bus.readByte(DEVICE_ADDR, STATUS_REG)
+          if (status & STATUS_BITS.AVAILABLE) {
+            if (!active && (status & STATUS_BITS.DET)) {
+              active = true
+              onActive().then(
+                () => {
+                  active = false
+                }
+              )
+            }
+            await bus.writeByte(DEVICE_ADDR, STATUS_REG, status & (~MASKS.CLEAR))
+          }
+        },
+        interval
+      )
+      return timer
+    }
+
+    let timer = start(100)
+
+    parentPort.on('message', (message) => {
+      if (timer) {
+        clearInterval(timer)
+        if (message > 0) {
+          console.log(`Polling interval set to ${message} ms`)
+          timer = start(message)
+        } else {
+          console.log('Polling stopped')
+        }
+      }
+    })
+    console.log('Worker started')
+  }
+
   init()
 }
 
